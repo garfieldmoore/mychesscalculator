e82f7428c4ef0b05a2a88ecbc8985806c3c4afa0
garfieldmoore <garfieldmoore@gmail.com>
2017-04-12 21:46:23 +0100

    added feedback form

--- js/app.js
+++ js/app.js
@@ -11,6 +11,34 @@ function closeFeedback() {
 }
 
 function sendFeedback(){
+  var name = $('#name').val();
+  var title = $('#title').val();
+  var email = $('#email').val();
+  var feedback = $('#feedback_message').val();
+
+  console.log('name ' + name);
+  console.log('title ' + title);
+  console.log('email ' + email);
+  console.log('message ' + feedback);
+
+  var body = {name:'garfield', title:'test message', email:'garfieldmoore@gmail.com', message:'this is a test'};
+  // var response = $.ajax({
+  //            url: "http://30000" + "/myapi/createIssue",
+  //            method: "post",
+  //            dataType:'json',
+  //            data: JSON.stringify(body),
+  //            headers: {
+  //             'Content-Type': 'text/plain',
+  //             'X-XSRF-TOKEN' : XSRFToken,
+  //             'Cookie': 'workgroup_session_id='+workgroupSessionId+';XSRF-TOKEN='+XSRFToken
+  //            },
+  //            success:function(response){
+  //                 alert("success");
+  //             },
+  //             error: function(XMLHttpRequest, textStatus, errorThrown) {
+  //                 alert("Status: " + textStatus); alert("Error: " + errorThrown);
+  //             }
+  //         });
 
   closeFeedback();
 }
