bf9a813fd19a747ef2f93022351c0f957ea03e64
garfieldmoore <garfieldmoore@gmail.com>
2017-04-13 11:09:13 +0100

    cleaned code

--- js/app.js
+++ js/app.js
@@ -23,19 +23,11 @@ function sendFeedback(){
   console.log('message ' + feedback);
 
   var body = {name:'garfield', title:'test message', email:'garfieldmoore@gmail.com', message:'this is a test'};
-  // $('#contact_results').append('Thanks - We have recieved your feedback and will review it within 24 hours')
-//http://dojodigital.co.uk/sendEmail.php?message=final%20testmessage2&subject=finaltestsubject
-// var XSRFToken='KbyUmhTLMpYj7CD2di7JKP1P3qmLlkPt';
-// var workgroupSessionId='KbyUmhTLMpYj7CD2di7JKP1P3qmLlkPt'
   var response = $.ajax({
              url: "http://dojodigital.co.uk/sendEmail.php?subject=" + title + "&message="+ feedback,
              method: "post",
-            //  dataType:'json',
-            //  data: JSON.stringify(body),
              headers: {
               'Content-Type': 'text/plain',
-              // 'X-XSRF-TOKEN' : XSRFToken,
-              // 'Cookie': 'workgroup_session_id='+workgroupSessionId+';XSRF-TOKEN='+XSRFToken
              },
              success:function(response){
                   alert("success");
