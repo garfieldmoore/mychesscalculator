0a2b0a7be6a6af93f9f95ce143aac98a29f665c1
garfieldmoore <garfieldmoore@gmail.com>
2017-05-09 15:37:26 +0100

    added feeback form and navbar

---
+++ js/Controllers/CustomerFeedbackController.js
@@ -0,0 +1,7 @@
+'use strict';
+
+app.controller('CustomerFeedbackController',
+    function CustomerFeedbackController($scope) {
+        console.log('Enter customer feedback controller');
+        
+});
--- js/app.js
+++ js/app.js
@@ -4,25 +4,21 @@ var app = angular.module('ratingsApp', ['ngRoute']);
 
 app.config(function($routeProvider) {
 
-    $routeProvider.when('/ecfcalculator', {
-        templateUrl: 'templates/ChessCalculator.html',
-        controller: 'RatingCalculator2Controller'
-    });
+    // $routeProvider.when('/ecfcalculator', {
+    //     templateUrl: 'templates/ChessCalculator.html',
+    //     controller: 'RatingCalculator2Controller'
+    // });
+
+    $routeProvider.when('/customerFeedback', {
+      templateUrl: 'templates/customerFeedback.html',
+      controller: 'CustomerFeedbackController'
+    })
 
     $routeProvider.otherwise({
         redirectTo: '/ecfcalculator'
     });
 
 });
-
-
-function openFeedback() {
-    $('#feedbackForm').show();
-}
-
-function closeFeedback() {
-    $('#feedbackForm').hide();
-}
 
 function sendFeedback() {
     var name = encodeURI($('#name').val());
